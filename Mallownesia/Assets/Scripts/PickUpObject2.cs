dd12e8d9c43f652f4a1e4dbc3c65df2d3c242256
gloomii-kat <kat.mashego24@gmail.com>
2025-11-09 00:48:29 +0200

    Notes & particle
    Particle system fixing book and invite

--- Mallownesia/Assets/Scripts/PickUpObject2.cs
+++ Mallownesia/Assets/Scripts/PickUpObject2.cs
@@ -11,6 +11,8 @@ public class PickUpObject2 : MonoBehaviour
     public GameObject key1;
 
     private bool nearBook = false;
+    public ParticleSystem bookParticleSystem; // Reference to the book's particle system
+    public Light bookLight; // Reference to the book's light
 
     private void Awake()
     {
@@ -23,18 +25,21 @@ public class PickUpObject2 : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-           if (nearBook)
+            if (nearBook)
             {
                 Debug.Log("Interacted with book!");
                 if (bookPanel != null) bookPanel.SetActive(true);
                 if (interact != null) interact.gameObject.SetActive(false);
+
+                // Stop the particle system when book panel is opened
+                StopBookParticleSystem();
+                StopBookLight();
             }
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-
         if (other.CompareTag("Book"))
         {
             if (interact != null)
@@ -45,6 +50,15 @@ public class PickUpObject2 : MonoBehaviour
                 Debug.Log("BOOK OPEN");
             }
             nearBook = true;
+
+            // Get the particle system from the book object
+            bookParticleSystem = other.GetComponentInChildren<ParticleSystem>();
+
+            // If we found a particle system and it's not playing, start it
+            if (bookParticleSystem != null && !bookParticleSystem.isPlaying)
+            {
+                bookParticleSystem.Play();
+            }
         }
     }
 
@@ -62,8 +76,25 @@ public class PickUpObject2 : MonoBehaviour
         if (bookPanel != null)
         {
             bookPanel.SetActive(false);
-            
+
+            // DON'T restart the particle system when book panel is closed
+            // The particle system should remain stopped
         }
-            
+    }
+
+    private void StopBookParticleSystem()
+    {
+        if (bookParticleSystem != null && bookParticleSystem.isPlaying)
+        {
+            bookParticleSystem.Stop();
+        }
+    }
+
+    private void StopBookLight()
+    {
+        if (bookLight != null && bookLight.enabled)
+        {
+            bookLight.enabled = false;
+        }
     }
 }
